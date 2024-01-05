b3fc09d95a45fcfab5c42667f844701dbd8f0383
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-01-05 18:42:17 -0400

    feat: define commons folder and constants file

---
+++ server/src/common/constants.js
@@ -0,0 +1,15 @@
+const BAD_REQUEST = 400
+const NOT_FOUND = 404
+const INTERNAL_SERVER_ERROR = 500
+const SUCCESS = 200
+const UNAUTHORIZED = 401
+const OK = 'OK'
+
+module.exports = {
+    BAD_REQUEST,
+    INTERNAL_SERVER_ERROR,
+    SUCCESS,
+    NOT_FOUND,
+    UNAUTHORIZED,
+    OK
+}
